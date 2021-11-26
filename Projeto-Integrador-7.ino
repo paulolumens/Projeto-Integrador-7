d8fc538d1cd0e805792204f0de7b9a4ef791a7de
Paulo R. R. Furlan <32108797+paulolumens@users.noreply.github.com>
2021-11-26 07:35:06 -0300

    Add files via upload

--- Projeto-Integrador-7.ino
+++ Projeto-Integrador-7.ino
@@ -1,43 +1,92 @@
 /*
-
   sistema adaptado por Paulo Furlan 12/2018
   Revisado para projeto integrador 7
   Univesp
   27/10/2021 -> Paulo Furlan
   Implementado opcoes para AP ou STA
-
+  Revisado:
+  05/11/2021
+  Revisado:
+  24/11/2021
 */
-//=============declaracao includes==========================
+//============= declaracao includes ==========================
 #include <ESP8266WiFi.h>
 #include <ESP8266LLMNR.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
 
+//====================== cria objeto servidor porta 80 =====================
+ESP8266WebServer web_server(80);    //cria objeto para o servidor na porta 80
+WiFiClient cliente; //Cria um cliente seguro (para ter acesso ao HTTPS)
+//======================= declarar variaveis do sistema ====================
+int pinoD5 = 4; //GPIO04 = D2
+int pinoD8 = 5; //GPIO05 = D1
+int pinoMODE = 13; //GPIO13 = D7
+int led_online = 12; //GPIO12 = D6
+int led_offline = 14; //GPIO14 = D5
+String  est_lamp1 = "aguardando";
+String  est_lamp2 = "aguardando";
+int lamp1;
+int lamp2;
+long rssi;
+boolean flag_lamp1 = false;
+boolean flag_lamp2 = false;
 //=================== Escolher Modo de Operacao ==============
-#ifndef MODE
-#define MODE 1  ///1 para AP 0 para cliente
-#endif
+boolean MODE = false; //true para AP, false para cliente
 //============================================================
-char* ssid = "";    //nome da rede que ira conectar
-char* password = "";//senha da
+char *ssid = "sua_rede";    //nome da rede que ira conectar
+char *password = "sua_senha";//senha da
 
 
 
 void config_rede() {
+  MODE = digitalRead(pinoMODE);
   if (MODE) {//Mode AP
+    digitalWrite(led_online, 0);
+    digitalWrite(led_offline, 1);
     ssid = "Projeto7";    //nome da rede que ira conectar
     password = "univesp";//senha da
     IPAddress ip(192, 168, 4, 1);
+    delay(100);
     IPAddress gateway(192, 168, 4, 1);
+    delay(100);
     IPAddress subnet(255, 255, 255, 0);
-    IPAddress primaryDNS(8, 8, 8, 2);
-    IPAddress secondaryDNS(8, 8, 8, 3);
+    delay(100);
+    IPAddress primaryDNS(181, 213, 132, 2);
+    delay(100);
+    IPAddress secondaryDNS(181, 213, 132, 3);
+    delay(100);
     WiFi.mode(WIFI_AP);
+    delay(100);
     WiFi.softAP(ssid, password);
+    delay(100);
     WiFi.softAPConfig(ip, gateway, subnet);
     delay(1000);
+    // Start LLMNR responder
+    LLMNR.begin("AP-Univesp");
+    Serial.println("LLMNR responder started");
+    // Start HTTP server
+    web_server.on("", handle_http_root);
+    web_server.onNotFound(handle_http_not_found);
+    web_server.on(" ", handle_http_root);
+    web_server.on("/", handle_http_root);
+    web_server.on("/1", funcao1);
+    web_server.begin();
+    Serial.println("iniciando coneccao");
+    digitalWrite(led_online, 0);
+    digitalWrite(led_offline, 1);
+    /*
+      // Wait for connection
+      while (WiFi.status() != WL_CONNECTED) {
+      digitalWrite(led_online, 1);
+      delay(250);
+      Serial.print(".");
+      digitalWrite(led_online, 0);
+      delay(250);
+      }
+    */
     Serial.println("===============================================");
-    Serial.println("MODO AP");
+    Serial.println("=================== MODO AP ===================");
     Serial.print("ssid: ");
     Serial.println(ssid);
     Serial.print("password: ");
@@ -51,19 +100,57 @@ void config_rede() {
     Serial.println("===============================================");
 
   } else {
-    ssid = "Sua Rede";    //nome da rede que ira conectar
-    password = "Sua Senha";//senha da rede
-    IPAddress ip(192, 168, 0, 27);
+    digitalWrite(led_online, 0);
+    digitalWrite(led_offline, 1);
+    ssid = "Sua_Rede";    //nome da rede que ira conectar
+    password = "Sua_Senha";//senha da rede
+    IPAddress ip(192, 168, 0, 4);
+    delay(100);
     IPAddress gateway(192, 168, 0, 1);
+    delay(100);
     IPAddress subnet(255, 255, 255, 0);
+    delay(100);
     IPAddress primaryDNS(181, 213, 132, 2);
+    delay(100);
     IPAddress secondaryDNS(181, 213, 132, 3);
+    delay(100);
     WiFi.mode(WIFI_STA);
+    delay(100);
     WiFi.begin(ssid, password);
+    delay(100);
     WiFi.config(ip, gateway, subnet);
+
     delay(1000);
+    // Start LLMNR responder
+    LLMNR.begin("STA-Unisvesp");
+    Serial.println("LLMNR responder started");
+    // Start HTTP server
+    web_server.on("", handle_http_root);
+    web_server.onNotFound(handle_http_not_found);
+    web_server.on(" ", handle_http_root);
+    web_server.on("/", handle_http_root);
+    web_server.on("/1", funcao1);
+    web_server.begin();
+    Serial.println("iniciando coneccao");
+    digitalWrite(led_online, 0);
+    digitalWrite(led_offline, 1);
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      digitalWrite(led_online, 1);
+      delay(250);
+      Serial.print(".");
+      digitalWrite(led_online, 0);
+      delay(250);
+    }
+    if (cliente.connect("sheets.googleapis.com", 443) == true) {
+      //Tenta se conectar ao servidor do Google APIs na porta 443 (HTTPS)
+      Serial.println("Conseguiu conexão com Servidor Google APIs");
+    }
+    else {
+      Serial.println("Falha na conexão com Servidor Google APIs");
+    }
     Serial.println("===============================================");
-    Serial.println("MODO CLIENTE");    
+    Serial.println("=============== MODO CLIENTE ==================");
     Serial.print("ssid: ");
     Serial.println(ssid);
     Serial.print("password: ");
@@ -80,23 +167,8 @@ void config_rede() {
   return;
 }
 
-//====================== cria objeto servidor porta 80 =====================
-ESP8266WebServer web_server(80);    //cria objeto para o servidor na porta 80
 
-int pinoD5 = 4;
-int pinoD8 = 5;
-int led_online = 12;
-int led_offline = 14;
-String  est_lamp1 = "aguardando";
-String  est_lamp2 = "aguardando";
-int lamp1;
-int lamp2;
-long rssi;
-boolean flag_lamp1 = false;
-boolean flag_lamp2 = false;
-
-
-//=============== criacao da pagina========================================================
+//=============== criacao da pagina Original========================================================
 String getPage() {
   String page = "<html lang=pt-BR><head><meta http-equiv='refresh' content='12'/>";
   page += "<title>Univesp</title>";
@@ -178,14 +250,11 @@ String getPage() {
 
   page += "}";
   page += "</script>";
-
-
-
-
   page += "</html>";
 
   return page;
 }
+
 
 //========fucao erro no carregamento===================
 void handle_http_not_found() {
@@ -200,6 +269,7 @@ void handle_http_not_found() {
 void handle_http_root() {
   rssi = WiFi.RSSI();
   handleRoot();
+  //String message = getPage();
   String message = getPage();
   web_server.send(200, "text/html", message);
   digitalWrite(led_online, 1);
@@ -220,12 +290,14 @@ void handleRoot() {
   if ( web_server.hasArg("LED") ) {
     handleSubmit();
   } else {
+    //web_server.send ( 200, "text/html", getPage() );
     web_server.send ( 200, "text/html", getPage() );
   }
 
   if ( web_server.hasArg("LED2") ) {
     handleSubmit();
   } else {
+    //web_server.send ( 200, "text/html", getPage() );
     web_server.send ( 200, "text/html", getPage() );
   }
 
@@ -274,10 +346,13 @@ void handleSubmit() {
     if (flag_lamp1) {
       // est_lamp1 = "ON";
       lamp1 = 1;
+      Serial.println("Acendeu a luz Garagem");
     } else if (!flag_lamp1) {
       // est_lamp1 = "OFF";
       lamp1 = 0;
+      Serial.println("Apagou a luz Garagem");
     }
+    //web_server.send ( 200, "text/html", getPage() );
     web_server.send ( 200, "text/html", getPage() );
   }
   //=============lamp2==============================
@@ -303,16 +378,20 @@ void handleSubmit() {
     digitalWrite(pinoD8, flag_lamp2);
     if (flag_lamp2) {
       // est_lamp2 = "aguardando";
+      Serial.println("Acendeu a luz Jardim");
       lamp2 = 1;
     } else if (!flag_lamp2) {
       // est_lamp2 = "aguardando";
+      Serial.println("Apagou a luz Jardim");
       lamp2 = 0;
     }
+    //web_server.send ( 200, "text/html", getPage() );
     web_server.send ( 200, "text/html", getPage() );
   }
 
   digitalWrite(led_online, 1);
   digitalWrite(led_offline, 0);
+  //web_server.send ( 200, "text/html", getPage() );
   web_server.send ( 200, "text/html", getPage() );
 }
 
@@ -322,20 +401,23 @@ void setup(void) {
   pinMode(pinoD8, OUTPUT);
   pinMode(led_online, OUTPUT);
   pinMode(led_offline, OUTPUT);
+  pinMode(pinoMODE, INPUT_PULLUP);
 
   Serial.begin(115200);
+
+
   config_rede();//chama funcao para configurar em modo AP ou STA
 
   // Start LLMNR responder
-  LLMNR.begin("esp8266");
-  Serial.println("LLMNR responder started");
+  //LLMNR.begin("esp8266");
+  //Serial.println("LLMNR responder started");
   // Start HTTP server
-  web_server.on("", handle_http_root);
-  web_server.onNotFound(handle_http_not_found);
-  web_server.on(" ", handle_http_root);
-  web_server.on("/", handle_http_root);
-  web_server.on("/1", funcao1);
-  web_server.begin();
+  //web_server.on("", handle_http_root);
+  //web_server.onNotFound(handle_http_not_found);
+  //web_server.on(" ", handle_http_root);
+  //web_server.on("/", handle_http_root);
+  //web_server.on("/1", funcao1);
+  //web_server.begin();
   Serial.println("HTTP server started");
   rssi = WiFi.RSSI();
   Serial.print("Sinal (RSSI):");
@@ -359,6 +441,17 @@ void setup(void) {
 
 void loop(void) {
   web_server.handleClient();
-
+  //getAppGoogle();
   delay(1000);
+}
+
+void getAppGoogle(){
+      if (cliente.connect("192.168.0.45", 80) == true) {
+      //Tenta se conectar ao servidor do Google APIs na porta 443 (HTTPS)
+      Serial.println("Conseguiu conexão com Servidor Clima IoT");
+    }
+    else {
+      Serial.println("Falha na conexão com Servidor Clima IoT");
+    }
+    return;
 }
